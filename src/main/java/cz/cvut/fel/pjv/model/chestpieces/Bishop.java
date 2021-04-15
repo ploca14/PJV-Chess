e7383eb6e5226c41bd61330d91bea886f4c02df9
Kryštof Müller <mullekry@fel.cvut.cz>
2021-04-15 16:11:18 +0200

    Bishop.java getLegalMoves(), Queen.java getLegalMoves()

--- src/main/java/cz/cvut/fel/pjv/model/chestpieces/Bishop.java
+++ src/main/java/cz/cvut/fel/pjv/model/chestpieces/Bishop.java
@@ -3,6 +3,7 @@ package cz.cvut.fel.pjv.model.chestpieces;
 import java.util.ArrayList;
 
 public class Bishop extends Chesspiece{
+    private String color;
 
     public Bishop(String color, Tile currentPosition) {
         super(color, currentPosition);
@@ -10,6 +11,51 @@ public class Bishop extends Chesspiece{
 
     @Override
     public ArrayList<Tile> getLegalMoves(Tile currentPosition, Tile[][] board) {
+        ArrayList<Tile> moves = new ArrayList<Tile>();
+
+        int x = currentPosition.getX();
+        int y = currentPosition.getY();
+
+        // top right direction
+        for (int i = 0; i < 10; i++) {
+            if(isOutOfRange(x+i,y-i)) { break; }
+            if(!isOccupied(board, x+i, y-i)) { moves.add(board[x+i][y-i]); }
+            if(isOccupied(board,x+i,y-i)) {
+                if (isTeammate(board,x+i,y-i,color)) { break; }
+                else { moves.add(board[x+i][y-i]); break; }
+            }
+        }
+
+        // top left direction
+        for (int i = 0; i < 10; i++) {
+            if(isOutOfRange(x-i,y-i)) { break; }
+            if(!isOccupied(board, x-i, y-i)) { moves.add(board[x+i][y-i]); }
+            if(isOccupied(board,x-i,y-i)) {
+                if (isTeammate(board,x-i,y-i,color)) { break; }
+                else { moves.add(board[x-i][y-i]); break; }
+            }
+        }
+
+        // bot left direction
+        for (int i = 0; i < 10; i++) {
+            if(isOutOfRange(x-i,y+i)) { break; }
+            if(!isOccupied(board, x-i, y+i)) { moves.add(board[x+i][y+i]); }
+            if(isOccupied(board,x-i,y+i)) {
+                if (isTeammate(board,x-i,y+i,color)) { break; }
+                else { moves.add(board[x-i][y+i]); break; }
+            }
+        }
+
+        // bot right direction
+        for (int i = 0; i < 10; i++) {
+            if(isOutOfRange(x+i,y+i)) { break; }
+            if(!isOccupied(board, x+i, y+i)) { moves.add(board[x+i][y+i]); }
+            if(isOccupied(board,x+i,y+i)) {
+                if (isTeammate(board,x+i,y+i,color)) { break; }
+                else { moves.add(board[x+i][y+i]); break; }
+            }
+        }
+
 
         return null;
     }
--- src/main/java/cz/cvut/fel/pjv/model/chestpieces/Queen.java
+++ src/main/java/cz/cvut/fel/pjv/model/chestpieces/Queen.java
@@ -1,14 +1,34 @@
 package cz.cvut.fel.pjv.model.chestpieces;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Queen extends Chesspiece {
+    private String color;
     public Queen(String color, Tile currentPosition) {
         super(color, currentPosition);
     }
 
     @Override
     public ArrayList<Tile> getLegalMoves(Tile currentPosition, Tile[][] board) {
+        ArrayList<Tile> moves = new ArrayList<Tile>();
+
+        int x = currentPosition.getX();
+        int y = currentPosition.getY();
+
+        // bishopmoves
+        ArrayList<Tile> bishopMoves = new ArrayList<Tile>();
+        Bishop b = new Bishop(color, currentPosition);
+        bishopMoves = b.getLegalMoves(currentPosition, board);
+
+        // rook moves
+        ArrayList<Tile> rookMoves = new ArrayList<Tile>();
+        Rook r = new Rook(color, currentPosition);
+        rookMoves = r.getLegalMoves(currentPosition, board);
+
+        // connect
+        moves.addAll(bishopMoves);
+        moves.addAll(rookMoves);
 
         return null;
     }
